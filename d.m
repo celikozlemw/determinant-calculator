250f161a9afc1a0b6131bfdee33a4e0cbdba516d
ÖZLEM <celik.ozlem.05062003@gmail.com>
2023-08-08 19:04:26 +0300

    Update d.m

--- d.m
+++ d.m
@@ -7,3 +7,13 @@ det = det(A);
 disp('matrix determinantı:');
 disp(det);
 ​
+%Yada
+
+
+A = input('matris girin:(örnek olarak2x2 için:[1 2; 3 4] 3x3 için:[1 2 3; 4 5 6; 7 8 9]) ');
+
+% determinantı hesaplayın
+det = detA(A);
+%yazdırın
+fprintf('Matris determinantı: %.2f\n', det);
+
